a1c9e2deb6edb5c618fb53ab8de17593dfb81469
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-05-08 23:20:23 +0530

    e

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,8 +9,8 @@
 <div class="flex flex-col min-h-screen items-center">
     <div class="flex flex-row justify-center gap-8 text-xl p-4">
         <a href="/">Key Pair</a>
-        <a href="/public">Encrypt</a>
-        <a href="/private">Decrypt</a>
+        <a href="/public" target="_blank">Encrypt</a>
+        <a href="/private" target="_blank">Decrypt</a>
     </div>
 
     <slot />
